c8cd4a5b576a0da750f3cb2f3d3b0fd7be013cce
Samuel Bravi <samuelbravi@gmail.com>
2026-05-14 14:22:22 +0200

    fix: call VectoreBase_Config inside main()

--- das-bootloader-sw/Core/Src/main.c
+++ das-bootloader-sw/Core/Src/main.c
@@ -60,6 +60,17 @@ void SystemClock_Config(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
+static void VectorBase_Config(void)
+{
+  /* The constant array with vectors of the vector table is declared externally in the
+   * c-startup code.
+   */
+  extern const unsigned long g_pfnVectors[];
+ 
+  /* Remap the vector table to where the vector table is located for this program. */
+  SCB->VTOR = (unsigned long)&g_pfnVectors[0];
+}
+
 /* USER CODE END 0 */
 
 /**
@@ -69,6 +80,8 @@ void SystemClock_Config(void);
 int main(void) {
 
     /* USER CODE BEGIN 1 */
+
+    VectorBase_Config();
 
     /* USER CODE END 1 */
 
@@ -154,17 +167,6 @@ void SystemClock_Config(void) {
 }
 
 /* USER CODE BEGIN 4 */
-
-static void VectorBase_Config(void)
-{
-  /* The constant array with vectors of the vector table is declared externally in the
-   * c-startup code.
-   */
-  extern const unsigned long g_pfnVectors[];
- 
-  /* Remap the vector table to where the vector table is located for this program. */
-  SCB->VTOR = (unsigned long)&g_pfnVectors[0];
-}
 
 /* USER CODE END 4 */
 
