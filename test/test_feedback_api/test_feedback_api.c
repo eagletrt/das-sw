5d231c20c8e25a15e23a5b89565a391fc4898a33
Samuel Bravi <samuelbravi@gmail.com>
2026-05-22 16:19:30 +0200

    fix: removed ASSERT inside for loops

--- test/test_feedback_api/test_feedback_api.c
+++ test/test_feedback_api/test_feedback_api.c
@@ -7,10 +7,6 @@
 
 extern struct FeedbackHandler feedback_api_handler;
 
-static enum FeedbackName feedback_at(uint8_t index) {
-    return (enum FeedbackName)index;
-}
-
 static enum FeedbackState valid_state_at(uint8_t index) {
     static const enum FeedbackState states[] = {
         FEEDBACK_STATE_LOW,
@@ -39,20 +35,28 @@ void test_feedback_api_init_should_return_ok(void) {
 }
 
 void test_feedback_api_init_should_set_all_feedbacks_to_error(void) {
+    uint8_t expected_states[FEEDBACK_NAME_COUNT];
+    uint8_t actual_states[FEEDBACK_NAME_COUNT];
+
     for (uint8_t i = 0U; i < (uint8_t)FEEDBACK_NAME_COUNT; i++) {
         feedback_api_handler.feedback_line_state[i] = FEEDBACK_STATE_LOW;
+        expected_states[i] = (uint8_t)FEEDBACK_STATE_ERROR;
     }
 
     enum FeedbackReturnCode rc = feedback_api_init();
 
     TEST_ASSERT_EQUAL_UINT8(FEEDBACK_RC_OK, rc);
 
+    // actual_states used for conversion enum --> uint8_t
     for (uint8_t i = 0U; i < (uint8_t)FEEDBACK_NAME_COUNT; i++) {
-        TEST_ASSERT_EQUAL_UINT8_MESSAGE(
-            FEEDBACK_STATE_ERROR,
-            feedback_api_handler.feedback_line_state[i],
-            "Each feedback state should be initialized to FEEDBACK_STATE_ERROR");
+        actual_states[i] = (uint8_t)feedback_api_handler.feedback_line_state[i];
     }
+
+    TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE(
+        expected_states,
+        actual_states,
+        FEEDBACK_NAME_COUNT,
+        "Each feedback state should be initialized to FEEDBACK_STATE_ERROR");
 }
 
 void test_feedback_api_get_state_should_return_saved_state_for_each_valid_feedback(void) {
@@ -60,7 +64,7 @@ void test_feedback_api_get_state_should_return_saved_state_for_each_valid_feedba
         enum FeedbackState expected_state = valid_state_at(i);
         feedback_api_handler.feedback_line_state[i] = expected_state;
 
-        enum FeedbackState actual_state = feedback_api_get_state(feedback_at(i));
+        enum FeedbackState actual_state = feedback_api_get_state((enum FeedbackName)i);
 
         TEST_ASSERT_EQUAL_UINT8_MESSAGE(
             expected_state,
@@ -95,32 +99,57 @@ void test_feedback_api_set_state_should_accept_each_valid_feedback_and_each_vali
         FEEDBACK_STATE_ERROR,
     };
 
+    enum {
+        valid_state_count = sizeof(valid_states) / sizeof(valid_states[0]),
+        test_case_count = FEEDBACK_NAME_COUNT * valid_state_count,
+    };
+
+    uint8_t return_codes[test_case_count];
+    uint8_t state_update_results[test_case_count];
+
+    uint8_t test_case_index = 0U;
+
     for (uint8_t feedback_index = 0U; feedback_index < (uint8_t)FEEDBACK_NAME_COUNT; feedback_index++) {
-        for (uint8_t state_index = 0U;
-             state_index < (uint8_t)(sizeof(valid_states) / sizeof(valid_states[0]));
-             state_index++) {
+        for (uint8_t state_index = 0U; state_index < (uint8_t)valid_state_count; state_index++) {
             enum FeedbackState expected_state = valid_states[state_index];
 
             enum FeedbackReturnCode rc = feedback_api_set_state(
-                feedback_at(feedback_index),
+                (enum FeedbackName)feedback_index,
                 expected_state);
 
-            TEST_ASSERT_EQUAL_UINT8_MESSAGE(
-                FEEDBACK_RC_OK,
-                rc,
-                "feedback_api_set_state() should return OK for valid feedback and valid state");
+            return_codes[test_case_index] = (uint8_t)rc;
 
-            TEST_ASSERT_EQUAL_UINT8_MESSAGE(
-                expected_state,
-                feedback_api_handler.feedback_line_state[feedback_index],
-                "feedback_api_set_state() should update the requested feedback state");
+            state_update_results[test_case_index] =
+                (feedback_api_handler.feedback_line_state[feedback_index] == expected_state)
+                    ? 1U
+                    : 0U;
+
+            test_case_index++;
         }
     }
+
+    TEST_ASSERT_EACH_EQUAL_UINT8_MESSAGE(
+        FEEDBACK_RC_OK,
+        return_codes,
+        test_case_count,
+        "feedback_api_set_state() should return OK for each valid feedback and valid state");
+
+    TEST_ASSERT_EACH_EQUAL_UINT8_MESSAGE(
+        1U,
+        state_update_results,
+        test_case_count,
+        "feedback_api_set_state() should update the requested feedback state for each valid feedback and valid state");
 }
 
 void test_feedback_api_set_state_should_reject_invalid_feedback_count(void) {
+    uint8_t expected_states[FEEDBACK_NAME_COUNT];
+    uint8_t actual_states[FEEDBACK_NAME_COUNT];
+
     for (uint8_t i = 0U; i < (uint8_t)FEEDBACK_NAME_COUNT; i++) {
-        feedback_api_handler.feedback_line_state[i] = valid_state_at(i);
+        enum FeedbackState state = valid_state_at(i);
+
+        feedback_api_handler.feedback_line_state[i] = state;
+        expected_states[i] = (uint8_t)state;
     }
 
     enum FeedbackReturnCode rc = feedback_api_set_state(
@@ -132,17 +161,27 @@ void test_feedback_api_set_state_should_reject_invalid_feedback_count(void) {
         rc,
         "feedback_api_set_state(FEEDBACK_NAME_COUNT, ...) should return FEEDBACK_RC_ERROR");
 
+    // actual_states used for conversion enum --> uint8_t
     for (uint8_t i = 0U; i < (uint8_t)FEEDBACK_NAME_COUNT; i++) {
-        TEST_ASSERT_EQUAL_UINT8_MESSAGE(
-            valid_state_at(i),
-            feedback_api_handler.feedback_line_state[i],
-            "Invalid feedback should not modify any valid feedback state");
+        actual_states[i] = (uint8_t)feedback_api_handler.feedback_line_state[i];
     }
+
+    TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE(
+        expected_states,
+        actual_states,
+        FEEDBACK_NAME_COUNT,
+        "Invalid feedback should not modify any valid feedback state");
 }
 
 void test_feedback_api_set_state_should_reject_feedback_after_count(void) {
+    uint8_t expected_states[FEEDBACK_NAME_COUNT];
+    uint8_t actual_states[FEEDBACK_NAME_COUNT];
+
     for (uint8_t i = 0U; i < (uint8_t)FEEDBACK_NAME_COUNT; i++) {
-        feedback_api_handler.feedback_line_state[i] = valid_state_at(i);
+        enum FeedbackState state = valid_state_at(i);
+
+        feedback_api_handler.feedback_line_state[i] = state;
+        expected_states[i] = (uint8_t)state;
     }
 
     enum FeedbackReturnCode rc = feedback_api_set_state(
@@ -155,15 +194,18 @@ void test_feedback_api_set_state_should_reject_feedback_after_count(void) {
         "feedback_api_set_state() should reject feedback values greater than FEEDBACK_NAME_COUNT");
 
     for (uint8_t i = 0U; i < (uint8_t)FEEDBACK_NAME_COUNT; i++) {
-        TEST_ASSERT_EQUAL_UINT8_MESSAGE(
-            valid_state_at(i),
-            feedback_api_handler.feedback_line_state[i],
-            "Invalid feedback should not modify any valid feedback state");
+        actual_states[i] = (uint8_t)feedback_api_handler.feedback_line_state[i];
     }
+
+    TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE(
+        expected_states,
+        actual_states,
+        FEEDBACK_NAME_COUNT,
+        "Invalid feedback should not modify any valid feedback state");
 }
 
 void test_feedback_api_set_state_should_reject_invalid_state_count(void) {
-    enum FeedbackName feedback = feedback_at(0U);
+    enum FeedbackName feedback = (enum FeedbackName)0U;
     feedback_api_handler.feedback_line_state[0] = FEEDBACK_STATE_LOW;
 
     enum FeedbackReturnCode rc = feedback_api_set_state(
@@ -182,7 +224,7 @@ void test_feedback_api_set_state_should_reject_invalid_state_count(void) {
 }
 
 void test_feedback_api_set_state_should_reject_state_after_count(void) {
-    enum FeedbackName feedback = feedback_at(0U);
+    enum FeedbackName feedback = (enum FeedbackName)0U;
     feedback_api_handler.feedback_line_state[0] = FEEDBACK_STATE_HIGH;
 
     enum FeedbackReturnCode rc = feedback_api_set_state(
