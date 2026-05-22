f011e3a8ecc204e2b1fcd9a526cf64b526303779
Samuel Bravi <samuelbravi@gmail.com>
2026-05-22 16:18:36 +0200

    feat: add Doxygen comments

--- Core/Inc/das/feedback-api.h
+++ Core/Inc/das/feedback-api.h
@@ -4,6 +4,30 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+/*!
+ * \brief Initializes the module
+ *
+ * \retval FEEDBACK_RC_OK if successful
+ * \retval FEEDBACK_RC_ERROR if fail
+ */
 enum FeedbackReturnCode feedback_api_init(void);
+
+/*!
+ * \brief  Get the current state of a feedback line
+ *
+ * \param feedback The feedback to query
+ *
+ * \retval enum FeedbackState of \p feedback
+ */
 enum FeedbackState feedback_api_get_state(enum FeedbackName feedback);
-enum FeedbackReturnCode feedback_api_set_state(enum FeedbackName feedback, enum FeedbackState value);
+
+/*!
+ * \brief  Set the current state of a feedback line
+ *
+ * \param feedback The feedback to be set
+ * \param state The state to set \p feedback to
+ *
+ * \retval FEEDBACK_RC_OK if successful
+ * \retval FEEDBACK_RC_ERROR if \p feedback or \p state are out of bounds
+ */
+enum FeedbackReturnCode feedback_api_set_state(enum FeedbackName feedback, enum FeedbackState state);
--- Core/Inc/das/feedback.h
+++ Core/Inc/das/feedback.h
@@ -9,44 +9,59 @@
 
 #if defined(DAS_FRONT)
 
+/*!
+ * \brief Names for checked shutdowns of DAS FRONT
+ */
 enum FeedbackName {
-    FEEDBACK_NAME_PILOT,
-    FEEDBACK_NAME_SX,
-    FEEDBACK_NAME_DX,
-    FEEDBACK_NAME_INERTIAL,
-    FEEDBACK_NAME_COUNT
+    FEEDBACK_NAME_PILOT,    /*!< Shutdown button for the pilot */
+    FEEDBACK_NAME_SX,       /*!< Mainhoop SX shutdown */
+    FEEDBACK_NAME_DX,       /*!< Mainhoop DX shutdown */
+    FEEDBACK_NAME_INERTIAL, /*!< Inertial shutdown */
+    FEEDBACK_NAME_COUNT     /*!< Total number of feedback lines */
 };
 
 #elif defined(DAS_REAR)
 
+/*!
+ * \brief Names for checked shutdowns of DAS REAR
+ */
 enum FeedbackName {
-    FEEDBACK_NAME_INTERLOCK,
-    FEEDBACK_NAME_ARMS_LINE,
-    FEEDBACK_NAME_COUNT
+    FEEDBACK_NAME_INTERLOCK, /*!< Interlock feedback */
+    FEEDBACK_NAME_ARMS_LINE, /*!< Arms line feedback signal */
+    FEEDBACK_NAME_COUNT      /*!< Total number of feedback lines */
 };
 
 #else
 #error "Either DAS_FRONT or DAS_REAR must be defined"
 #endif
 
-#define FEEDBACK_THRESHOLD_LOW 300
-#define FEEDBACK_THRESHOLD_HIGH 3900
+#define FEEDBACK_THRESHOLD_LOW (300)
+#define FEEDBACK_THRESHOLD_HIGH (3900)
 
+/*!
+ * \brief Possible return codes for feedback initialization function
+ */
 enum FeedbackReturnCode {
-    FEEDBACK_RC_OK,
-    FEEDBACK_RC_ERROR
+    FEEDBACK_RC_OK,   /*!< Initialization successful */
+    FEEDBACK_RC_ERROR /*!< Error during initialization */
 };
 
+/*!
+ * \brief Possible return codes for feedback status functions
+ */
 enum FeedbackState {
-    FEEDBACK_STATE_LOW,
-    FEEDBACK_STATE_IMPLAUSIBILITY,
-    FEEDBACK_STATE_HIGH,
-    FEEDBACK_STATE_ERROR,
-    FEEDBACK_STATE_COUNT
+    FEEDBACK_STATE_LOW,            /*!< Feedback line is low */
+    FEEDBACK_STATE_IMPLAUSIBILITY, /*!< Feedback line is on a borderline state */
+    FEEDBACK_STATE_HIGH,           /*!< Feedback line is high */
+    FEEDBACK_STATE_ERROR,          /*!< Error in reading feedback button states */
+    FEEDBACK_STATE_COUNT           /*!< Total number of feedback statuses */
 };
 
+/*!
+ * \brief Struct that handles all relevant feedback information
+ */
 struct FeedbackHandler {
-    EAGLETRT_VOLATILE enum FeedbackState feedback_line_state[FEEDBACK_NAME_COUNT];
+    EAGLETRT_VOLATILE enum FeedbackState feedback_line_state[FEEDBACK_NAME_COUNT]; /*!< Feedback button state array */
 };
 
 #endif // FEEDBACK_H
