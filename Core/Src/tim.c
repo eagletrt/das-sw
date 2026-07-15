7de76bbf42a9b62121515bc114c33fa53b9522c8
Samuel <132440896+samuelbravi11@users.noreply.github.com>
2026-07-15 11:03:09 +0200

    Update Core/Src/tim.c
    Co-authored-by: Alessandro Bridi <ale.bridi15@gmail.com>

--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -107,7 +107,7 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
          * \note Clock rate must be <= 4 MHz (from RM44SC0012B10F2F10 datasheet)
          *       Also, the interval between two consecutive conversions must be > 20 μs
          */
-        volatile HAL_StatusTypeDef status = HAL_SPI_Receive(hspi_encoder, encoder_raw_buf, 2, 100);
+        volatile HAL_StatusTypeDef status = HAL_SPI_Receive_IT(hspi_encoder, encoder_raw_buf, 2);
         if (status != HAL_OK) {
             (void)status;
         }
