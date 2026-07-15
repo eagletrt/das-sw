47d640e4d34843409dd3bf26a41118b2a24e8c2d
Samuel <132440896+samuelbravi11@users.noreply.github.com>
2026-07-15 13:44:08 +0200

    Update Core/Inc/das/acquisinator-api.h
    Co-authored-by: Antonio Gelain <61268285+Tonidotpy@users.noreply.github.com>

--- Core/Inc/das/acquisinator-api.h
+++ Core/Inc/das/acquisinator-api.h
@@ -31,4 +31,4 @@ struct AcquisinatorStrainGauge acquisinator_api_get_strain_gauge(enum Acquisinat
  * \retval ACQUISINATOR_RC_OK if successful
  * \retval ACQUISINATOR_RC_ERROR if \p acquisinator are out of bounds
  */
-enum AcquisinatorReturnCode acquisinator_api_set_values(enum AcquisinatorName acquisinator, struct AcquisinatorStrainGauge strain_gauge);
+enum AcquisinatorReturnCode acquisinator_api_set_strain_gauge(enum AcquisinatorName acquisinator, struct AcquisinatorStrainGauge strain_gauge);
