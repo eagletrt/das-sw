a0704f38f2dd10723ae4993838b183398a6538ee
Samuel Bravi <samuelbravi@gmail.com>
2026-07-20 13:14:54 +0200

    fix: comments #17

--- Core/Inc/das/encoder-api.h
+++ Core/Inc/das/encoder-api.h
@@ -15,8 +15,10 @@
 enum EncoderReturnCode encoder_api_init(void);
 
 /*!
- * \brief  Get the current angle of a encoder steering wheel
+ * \brief Get the current angle of a encoder steering wheel
  *
+ * \param encoder The encoder to query
+ * 
  * \retval float angle of \p encoder steering wheel
  */
 float encoder_api_get_angle(enum EncoderName encoder);
@@ -24,6 +26,7 @@ float encoder_api_get_angle(enum EncoderName encoder);
 /*!
  * \brief  Set the current state of a encoder line
  *
+ * \param encoder The encoder to be set
  * \param state The state to set \p encoder to
  *
  * \retval ENCODER_RC_OK if successful
